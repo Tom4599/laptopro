29da1dbd35dba934a3cf8e5247d29c848945c66e
Tom4599 <tomsaunier@orange.fr>
2018-10-03 15:08:28 +0200

    getdatabase

--- src/model/getDatabase.php
+++ src/model/getDatabase.php
@@ -2,7 +2,7 @@
 function getDatabase()
 {
     try {
-        $bdd = new PDO('mysql:host=127.0.0.1;dbname=laptopro','root', '');
+        $bdd = new PDO('mysql:host=82.64.28.83;dbname=work','work', 'workepsi');
         $bdd->exec("SET NAMES 'UTF8'");
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
