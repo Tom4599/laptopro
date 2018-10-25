7f614d787e5dd840d74c51463abb9b54815f3a74
zenyth34 <polo.rub@hotmail.fr>
2018-10-25 09:10:43 +0200

    admin debut

---
+++ admin/admin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include_once("../src/views/head.html");
+?>
+<body>
+<!-- Navigation -->
+<?php
+require_once("../src/views/navbar_admin.php")
+?>
+
+
+<!-- Page Content -->
+
+<?php
+require_once("../src/views/admin_annonces.php");
+?>
+
+<!-- Footer -->
+<?php
+require_once("../src/views/footer.php");
+?>
+
+<!-- Bootstrap core JavaScript -->
+<?php
+require_once("../src/views/scripts.php");
+?>
+</body>
+
+</html>
---
+++ src/views/navbar_admin.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+    <a class="navbar-brand" href="http://localhost/laptopro/index.php">Laptopro</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="http://localhost/laptopro/index.php">Acceuil <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="http://localhost/laptopro/admin.php">Statistiques <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="http://localhost/laptopro/admin_annonces.php">Annonces <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="http://localhost/laptopro/admin_user.php">Utilisateurs <span class="sr-only"></span></a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
